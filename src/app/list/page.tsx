dc2381bf7ffb81a96402785fe35d0fb3c88a46d0
AbirBokhtiar <abirbokhtiar107#gmail.com>
2025-07-16 12:33:39 +0600

    changed list route

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,6 +1,6 @@
 // /src/app/list/page.tsx
 import React, { Suspense } from 'react';
-import List from '../../components/List';
+import List from '../components/List';
 import Skeleton from '@/app/components/skeleton';
 
 export default function ListPage() {
